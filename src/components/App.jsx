7f4e8d75152b42cac1abdb1b209b94b8c5cfb699
Dan Scott <Dan.Scott@sse.com>
2021-08-09 13:32:55 +0100

    Waiting for image to load properly before animation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,47 +1,48 @@
+import { useState } from "react";
 import styled from "styled-components";
 import { Animate, AnimateGroup } from "react-simple-animate";
 
+const Container = styled.div`
+    min-width: 100vw;
+    min-height: 100vh;
+    margin: auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    text-align: center;
 
-function App() {
+    img {
+      max-width: 100vw;
+    }
 
-  const Container = styled.div`
-      min-width: 100vw;
-      min-height: 100vh;
-      margin: auto;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      flex-direction: column;
-      text-align: center;
+    a:link,
+    a:visited,
+    a:hover {
+      text-decoration: none;
+      color: #0D8426;
+      font-weight:bold;
 
-      img {
-        max-width: 100vw;
+      &.social {
+        color: rgba(91,161,248,1);
       }
+    }
+`;
 
-      a:link,
-      a:visited,
-      a:hover {
-        text-decoration: none;
-        color: #0D8426;
-        font-weight:bold;
+const App = () => {
 
-        &.social {
-          color: rgba(91,161,248,1);
-        }
-      }
-  `;
+  const [loaded, setLoaded] = useState(false);
 
   const Twitter = () => <a className="social" href="https://twitter.com/CoderDan" target="_blank" rel="noreferrer">Twitter</a>
   const LinkedIn = () => <a className="social" href="https://linkedin.com/in/iDanScott" target="_blank" rel="noreferrer">LinkedIn</a>
   const Github = () => <a className="social" href="https://github.com/CoderDan1" target="_blank" rel="noreferrer">Github</a>
 
   return (
-
     <Container>
       <div>
-        <AnimateGroup play>
+        <AnimateGroup play={loaded}>
           <Animate start={{ opacity: 0, transform: 'translateY(10px)'}} end={{ opacity: 1, transform: 'translateY(0)'}} sequenceIndex={0} duration={1}>
-            <img src="/logo-512.png" alt="logo" />
+            <img src="/logo-512.png" alt="logo" onLoad={() => setLoaded(true)} />
           </Animate>
           <Animate start={{ opacity: 0, transform: 'translateY(10px)'}} end={{ opacity: 1, transform: 'translateY(0)'}} sequenceIndex={1} duration={1}>
             <p>Something new is on the horizon, watch this space.</p>
